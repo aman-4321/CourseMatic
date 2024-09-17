b3bd62d0cc22e99c61a162eda21a112ae3feb47f
Utkarsh Patel <pateluttkarsh697@gmail.com>
2024-09-17 13:11:28 +0530

    router setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Dashboard from './components/Dashboard.tsx';
+import Project from './components/Project.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard/>,
+  },
+  {
+    path: "/projectID",
+    element: <Project/>,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
