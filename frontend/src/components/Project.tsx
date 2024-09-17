2a1d3b15ae287c5f27242e5f80aee1d000883dba
ishantchauhan710 <ishantchauhan710@gmail.com>
2024-09-17 15:03:15 +0530

    Editor added

--- frontend/src/components/Project.tsx
+++ frontend/src/components/Project.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import Editor from "./Editor";
 
 export default function Project() {
   return (
     <div>
-      Project Page
+      <Editor />
     </div>
-  )
+  );
 }
